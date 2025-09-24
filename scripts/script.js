b3dbca3b85ccd4350768746b52d0512d788ab87c
Daphne <daphnejager02@gmail.com>
2025-09-24 12:00:36 +0200

    menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
 // JavaScript Document
 console.log("hi");
 
+let menuButton = document.querySelector("button")
+
+menuButton.addEventListener("click", toggleButton)
+
+function toggleButton() {
+  menuButton.classList.toggle("is-open")
+}
